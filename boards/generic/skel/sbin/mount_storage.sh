6de5ecd5b7de23584346e2841b3f2e95180bb805
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2014-03-14 00:20:00 +0100

    base: automatically unmount storage devices

--- boards/generic/skel/sbin/mount_storage.sh
+++ boards/generic/skel/sbin/mount_storage.sh
@@ -7,19 +7,34 @@ if test -z "$MDEV"; then
 fi
 
 mntpt=/mnt/$MDEV
-mkdir -p $mntpt
-fstype=`blkid "/dev/$MDEV" | sed 's,.*TYPE="\(.*\)",\1,;t;d'`
-if test "$fstype" = "ntfs" -a -e /sbin/mount.ntfs-3g; then
-    mount.ntfs-3g /dev/$MDEV $mntpt
-elif test -n "$fstype"; then
-    mount -t "$fstype" /dev/$MDEV $mntpt    
-else
-    mount /dev/$MDEV $mntpt
-fi
 
-if test "$MDEV" = @MKR_SWAP_DEV@; then
-    test -e $mntpt/swap || dd if=/dev/zero of=/mnt/swap bs=1M count=500
-    mkswap $mntpt/swap
-    swapon $mntpt/swap
-    mount -o remount,size=500M /tmp
-fi
+case "$ACTION" in
+remove)
+    if test "$MDEV" = @MKR_SWAP_DEV@; then
+	mount -o remount,size=64M /tmp
+	swapoff $mntpt/swap
+    fi
+    fstype=`sed 's,^/dev/'"$MDEV"' [^ ]* \([^ ]*\).*$,\1,;t;d' /proc/mounts`
+    umount -f "/dev/$MDEV"
+    test -n "$fstype" && modprobe -r "$fstype"
+    ;;
+
+add)
+    mkdir -p $mntpt
+    fstype=`blkid "/dev/$MDEV" | sed 's,.*TYPE="\(.*\)",\1,;t;d'`
+    if test "$fstype" = "ntfs" -a -e /sbin/mount.ntfs-3g; then
+	mount.ntfs-3g /dev/$MDEV $mntpt
+    elif test -n "$fstype"; then
+	mount -t "$fstype" /dev/$MDEV $mntpt
+    else
+	mount /dev/$MDEV $mntpt
+    fi
+
+    if test "$MDEV" = @MKR_SWAP_DEV@; then
+	test -e $mntpt/swap || dd if=/dev/zero of=/mnt/swap bs=1M count=500
+	mkswap $mntpt/swap
+	swapon $mntpt/swap
+	mount -o remount,size=500M /tmp
+    fi
+    ;;
+esac
