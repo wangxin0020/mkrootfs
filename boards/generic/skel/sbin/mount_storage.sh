dbdf8247357228caab49ec1f0bd5749261e6c4b5
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2014-03-13 08:26:00 +0100

    storage: use ntfs-3g for ntfs partitions if installed

--- boards/generic/skel/sbin/mount_storage.sh
+++ boards/generic/skel/sbin/mount_storage.sh
@@ -8,8 +8,14 @@ fi
 
 mntpt=/mnt/$MDEV
 mkdir -p $mntpt
-fsopt=`blkid "/dev/$MDEV" | sed 's,.*TYPE="\(.*\)",-t \1,;t;d'`
-mount $fsopt /dev/$MDEV $mntpt
+fstype=`blkid "/dev/$MDEV" | sed 's,.*TYPE="\(.*\)",\1,;t;d'`
+if test "$fstype" = "ntfs" -a -e /sbin/mount.ntfs-3g; then
+    mount.ntfs-3g /dev/$MDEV $mntpt
+elif test -n "$fstype"; then
+    mount -t "$fstype" /dev/$MDEV $mntpt    
+else
+    mount /dev/$MDEV $mntpt
+fi
 
 if test "$MDEV" = @MKR_SWAP_DEV@; then
     test -e $mntpt/swap || dd if=/dev/zero of=/mnt/swap bs=1M count=500
