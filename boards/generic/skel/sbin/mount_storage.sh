4464916d1acb4bea1612472076009cf14d2c6b92
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2014-08-16 19:02:03 +0200

    base: fix swap creation

--- boards/generic/skel/sbin/mount_storage.sh
+++ boards/generic/skel/sbin/mount_storage.sh
@@ -31,7 +31,7 @@ add)
     fi
 
     if test "$MDEV" = @MKR_SWAP_DEV@; then
-	test -e $mntpt/swap || dd if=/dev/zero of=/mnt/swap bs=1M count=500
+	test -e $mntpt/swap || dd if=/dev/zero of=$mntpt/swap bs=1M count=500
 	mkswap $mntpt/swap
 	swapon $mntpt/swap
 	mount -o remount,size=500M /tmp
