db26e41bed2a78c23572f08c910c54681674e699
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2016-06-21 01:20:25 +0200

    skeleton: automatically check filesystems before mounting
    For non journaling file systems, fat and ext2.

--- boards/generic/skel/sbin/mount_storage.sh
+++ boards/generic/skel/sbin/mount_storage.sh
@@ -23,6 +23,20 @@ remove)
 add)
     mkdir -p $mntpt
     fstype=`blkid "/dev/$MDEV" | sed 's,.*TYPE="\([^"]*\)".*,\1,;t;d'`
+
+    case "$fstype" in
+	*fat*)
+	    if test -x /usr/sbin/fsck.fat; then
+		/usr/sbin/fsck.fat -waV "/dev/$MDEV" || :
+	    fi
+	    ;;
+	ext2)
+	    if test -x /usr/sbin/e2fsck; then
+		/usr/sbin/e2fsck -p "/dev/$MDEV" || :
+	    fi
+	    ;;
+    esac
+
     if test "$fstype" = "swap"; then
 	if swapon /dev/$MDEV; then
 	    size=`awk '/^\/dev\/'"$MDEV"'/ { print $3 }' /proc/swaps`
@@ -38,7 +52,12 @@ add)
     fi
 
     if test "$MDEV" = @MKR_SWAP_DEV@; then
-	test -e $mntpt/swap || dd if=/dev/zero of=$mntpt/swap bs=1M count=500
+	test -e $mntpt/swap || {
+	    if [ ! -e /dev/zero ]; then
+		mknod /dev/zero c 1 5
+	    fi
+	    dd if=/dev/zero of=$mntpt/swap bs=1M count=500
+	}
 	mkswap $mntpt/swap
 	swapon $mntpt/swap
 	mount -o remount,size=500M /tmp
